356a19118f26e91f35815e465ee372db8101dc41
AndresWV <awallberg18@alumnos.utalca.cl>
2022-01-06 16:52:26 -0300

    update TicketWeb.cs
    se cambio la estructura del model

---
+++ SPCAPP.TicketWeb/SPCAPP.TicketWeb/Models/TicketWeb.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPCAPP.TicketWeb.Models
+{
+    public class TicketWeb
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Fecha{ get; set; }
+
+        public DateTime Hora { get; set; }
+
+        [Required(ErrorMessage = "El Folio es un campo obligatorio")]
+        public String Folio { get; set; }
+
+        [Required(ErrorMessage = "El Estado es un campo obligatorio")]
+        public String Estado { get; set; }
+
+        [Required(ErrorMessage = "El Cliente es un campo obligatorio")]
+        public String Cliente { get; set; }
+
+        public String Alias { get; set; }
+
+        public String Emailempresa { get; set; }
+
+        [Required(ErrorMessage = "El Contacto es un campo obligatorio")]
+        public String Contacto { get; set; }
+
+        public int Telefono1 { get; set; }
+
+        public int Telefono2 { get; set; }
+
+        public int Telefono3 { get; set; }
+
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "El Modo de contacto es un campo obligatorio")]
+        public String ModoContacto { get; set; }
+
+        [Required(ErrorMessage = "El Area de trabajo es un campo obligatorio")]
+        public String AreaTrabajo { get; set; }
+
+        [Required(ErrorMessage = "El Tipo de soporte es un campo obligatorio")]
+        public String TipoSoporte { get; set; }
+
+        [Required(ErrorMessage = "El Asignado es un campo obligatorio")]
+        public String Asignado { get; set; }
+
+        [Required(ErrorMessage = "El Proyecto es un campo obligatorio")]
+        public String Proyecto { get; set; }
+
+        public String ResumenTicket { get; set; }
+    }
+}
