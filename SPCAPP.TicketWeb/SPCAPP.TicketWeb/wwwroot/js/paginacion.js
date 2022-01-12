e82a8d7ac9b184050e9709e9aadcc6e260aad0b3
AndresWV <awallberg18@alumnos.utalca.cl>
2022-01-12 16:33:30 -0300

    paginacion
    se agrego paginacion  a la tabla

--- SPCAPP.TicketWeb/SPCAPP.TicketWeb/wwwroot/js/paginacion.js
+++ SPCAPP.TicketWeb/SPCAPP.TicketWeb/wwwroot/js/paginacion.js
@@ -1,8 +1,4 @@
 ﻿$(document).ready(function () {
     $('#dtBasicExample').DataTable();
     $('.dataTables_length').addClass('bs-select');
-    $('#dtBasicExample').DataTable({
-        "pagingType": "simple" // "simple" option for 'Previous' and 'Next' buttons only
-    });
-    $('.dataTables_length').addClass('bs-select');
 });
