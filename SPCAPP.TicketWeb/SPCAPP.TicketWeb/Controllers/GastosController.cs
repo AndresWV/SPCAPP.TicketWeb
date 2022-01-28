f9584635723f17d8a18e9e1ed3cde31265e17e00
AndresWV <awallberg18@alumnos.utalca.cl>
2022-01-28 11:37:41 -0300

    Tabla de gastos
    se incoorporo el script para manejar la tabla y la interfaz grafica

---
+++ SPCAPP.TicketWeb/SPCAPP.TicketWeb/Controllers/GastosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SPCAPP.TicketWeb.Data;
+using SPCAPP.TicketWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPCAPP.TicketWeb.Controllers
+{
+    public class GastosController : Controller
+    {
+        //conexion con la base de datos
+        private readonly ApplicationDbContext _context;
+        private readonly SPCTKContext _bd;
+        public SPCTKContext bd = new SPCTKContext();
+
+        public IActionResult Gastos()
+        {
+            return View();
+        }
+        public IActionResult GastosVista()
+        {
+
+            return PartialView("GastosVista", new List<RendTk>());
+        }
+        public int nGastosLista()
+        {
+            var listaGastos = bd.RendTks;
+            return listaGastos.Count();
+        }
+        public IActionResult getGastosLista()
+        {
+            var listaGastos = bd.RendTks;
+            return Json(listaGastos);
+        }
+    }
+}
