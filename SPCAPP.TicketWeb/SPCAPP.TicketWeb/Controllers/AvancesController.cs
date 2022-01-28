123e3e3a02e4f1c02d92c3b3ec7b5e3445a2a07a
AndresWV <awallberg18@alumnos.utalca.cl>
2022-01-28 12:11:29 -0300

    División de vistas avances de edit
    se agrego 1 controller para manejar la vistas de avances, trabajo realizado y password

---
+++ SPCAPP.TicketWeb/SPCAPP.TicketWeb/Controllers/AvancesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPCAPP.TicketWeb.Controllers
+{
+    public class AvancesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AvancesVista()
+        {
+            return PartialView("AvancesVista");
+        }
+
+        [HttpGet]
+        public IActionResult passwordVista()
+        {
+            return PartialView("passwordVista");
+        }
+
+        [HttpGet]
+        public IActionResult trbRealizadoVista()
+        {
+            return PartialView("trbRealizadoVista");
+        }
+    }
+}
