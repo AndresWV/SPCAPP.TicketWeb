ab0943232980318061cd16ece45a28dc63f3f4ce
AndresWV <awallberg18@alumnos.utalca.cl>
2022-02-02 15:14:21 -0300

    Corrección visual avances
    se agrego un margin para separar el checkbox

--- SPCAPP.TicketWeb/SPCAPP.TicketWeb/wwwroot/js/editModal.js
+++ SPCAPP.TicketWeb/SPCAPP.TicketWeb/wwwroot/js/editModal.js
@@ -1,4 +1,3 @@
-﻿console.log("llegaaaaa");
-$("#trbReaVist").load("/Avances/AvancesVista");
-$("#avancesVist").load("/Avances/passwordVista");
-$("#passVist").load("/Avances/AvancesVista");
+﻿$("#trbReaVist").load("/Avances/trbRealizadoVista");
+$("#passVist").load("/Avances/passwordVista");
+$("#avancesVist").load("/Avances/AvancesVista");
